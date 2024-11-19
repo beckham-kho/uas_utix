ec948c77b2532c270a220eeb4aea9c64c7886ed7
beckham-kho <beckham44997@gmail.com>
2024-11-19 18:12:00 +0700

    feat(splash screen): add splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,1 +1,56 @@
-st
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => const HomeScreen())
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(22, 22, 33, 1),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 250,
+              child: Image.asset('img/logo_utix.png'),
+            ),
+            const Text(
+              'Pick. Tap. Enjoy',
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromRGBO(247, 67, 70, 1),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
